a406480d0394b54893016a29297c83b61bdee317
JonSnowbd <vasti009@gmail.com>
2023-03-06 16:51:56 -0400

    Major changes to schmove api

--- global/schmove.gd
+++ global/schmove.gd
@@ -1,8 +1,13 @@
 extends Node
 
-const WipeCircle = preload("res://addons/schmove/material/circle_wipe.tres")
+## Ran on a deferred call after a scene switch via Schmove api
+signal DeferredSceneTransition
+signal StateUpdated
+signal Logged(message: String, log_type: int)
 
-var _uniques: Dictionary = {}
+const WipeCircle = preload("res://addons/schmove/material/circle_wipe.tres")
+const WipeStickGlitch = preload("res://addons/schmove/material/stick_wipe.tres")
+
 var _preloaded: Dictionary = {}
 var _launched: Array[SchmoveExporter2D] = []
 var _jail: Dictionary = {}
@@ -32,6 +37,8 @@ func preload_level(path: String):
 		return
 	_preloaded[path] = true
 	ResourceLoader.load_threaded_request(path)
+	StateUpdated.emit()
+	Logged.emit("Preload: "+path, 0)
 
 ## If you're making more levels than you have ram, you can flush
 ## this out at your judgement.
@@ -39,38 +46,51 @@ func preload_flush():
 	_preloaded.clear()
 
 var _t_level: String = ""
-var _t_nodes: Array[SchmoveExporter2D] = []
-var _t_launchers: Array[SchmoveExporter2D] = []
-var _t_importers: Array[SchmoveImporter2D] = []
+var _t_launched: Array[SchmoveExporter2D] = []
 var _t_keyword: String = ""
-var _using_wipe: bool = false
+var _t_using_wipe: bool = false
+var _t_shuffle: bool = false
 
-func _t_dig(node: Node):
-	if node is SchmoveImporter2D: 
-		return
-	for c in node.get_children():
-		if c is SchmoveExporter2D:
-			_t_nodes.append(c)
-		else:
-			_t_dig(c)
-func _i_dig(node: Node):
-	for c in node.get_children():
-		if c is SchmoveImporter2D:
-			_t_importers.append(c)
-		else:
-			_i_dig(c)
+func _emit_transition():
+	DeferredSceneTransition.emit()
+
+
+func _get_all_exporters(node: Node, depth: int = 0) -> Array[SchmoveExporter2D]:
+	var max_depth = ProjectSettings.get_setting("application/schmove/search_depth", 2)
+	if depth > max_depth:
+		return []
+	var exports: Array[SchmoveExporter2D] = []
+	if node is SchmoveExporter2D:
+		exports.append(node)
+	else:
+		for c in node.get_children():
+			exports.append_array(_get_all_exporters(c, depth+1))
+	return exports
+
+func _get_all_importers(node: Node, depth: int = 0) -> Array[SchmoveImporter2D]:
+	var max_depth = ProjectSettings.get_setting("application/schmove/search_depth", 2)
+	if depth > max_depth:
+		return []
+	var exports: Array[SchmoveImporter2D] = []
+	if node is SchmoveImporter2D:
+		exports.append(node)
+	else:
+		for c in node.get_children():
+			exports.append_array(_get_all_importers(c, depth+1))
+	return exports
 
 ## Schmove transitions are like a state machine, or imgui if you've used it.
 ## Begin a transition, use transition_* methods to customize, then finish
 ## with finish_transition or cancel with cancel_transition
 func begin_transition(new_level_path: String):
 	_t_level = new_level_path
-	
+	StateUpdated.emit()
 
 ## Importers can be locked behind a keyword. A target import slot is used only if
 ## their keywords match the transition keyword, IF either are set to anything but ""
 func transition_keyword(key: String):
 	_t_keyword = key
+	StateUpdated.emit()
 
 ## Launches a node to be taken with the transition. Unlaunched, unimportant nodes
 ## will be deleted as is the normal behaviour for godot nodes.
@@ -82,13 +102,39 @@ func transition_launch(target: Node):
 	var real = get_real_node(target)
 	if real != null:
 		real.get_parent().remove_child(real)
-		_t_launchers.append(get_exporter_node(real))
+		_t_launched.append(get_exporter_node(real))
+		StateUpdated.emit()
 		return
 	print("SCHMOVE: Failed to launch "+target.name+" as we could not find a SchmoveExporter2D in the children, and it itself is not a SchmoveExporter2D.")
 
+## Launches every node that is in the given jail keyword. If you called Schmove.jail_node(x, "party")
+## you can launch everything in that "party" word with this.
+func transition_launch_jail(keyword = ""):
+	if _jail.has(keyword):
+		for n in _jail[keyword]:
+			_t_launched.append(n)
+		_jail[keyword].clear()
+		StateUpdated.emit()
+
+## Semi expensive depending on scene depth, launches every node that has the given nickname.
+func transition_launch_by_name(name: String):
+	var scene = get_tree().current_scene
+	var exporters = _get_all_exporters(scene)
+	for ex in exporters:
+		if ex.Nickname == name:
+			print("Launching via nickname")
+			transition_launch(ex)
+	StateUpdated.emit()
+
+func transition_shuffle():
+	_t_shuffle = true
+	StateUpdated.emit()
+
 func transition_wipe(shader: ShaderMaterial, duration: float):
-	$Curtain.prime_transition(shader, duration)
-	_using_wipe = true
+	var tex: Image = get_viewport().get_texture().get_image()
+	$Curtain.prime_transition(shader, duration, ImageTexture.create_from_image(tex))
+	_t_using_wipe = true
+	StateUpdated.emit()
 
 ## Schmove transitions are like a state machine, or imgui if you've used it.
 ## Begin a transition, use transition_* methods to customize, then finish
@@ -96,9 +142,8 @@ func transition_wipe(shader: ShaderMaterial, duration: float):
 func finish_transition():
 	var start_stamp = Time.get_ticks_msec()
 		
-	if _using_wipe:
-		var tex: Image = get_viewport().get_texture().get_image()
-		$Curtain.run_transition(ImageTexture.create_from_image(tex))
+	if _t_using_wipe:
+		$Curtain.run_transition()
 	
 	# Handle loading the next level, or using the cached version
 	var target_level
@@ -113,31 +158,53 @@ func finish_transition():
 	var target_instance = target_level.instantiate()
 	var old_instance = get_tree().current_scene
 	
-	# Find all the schmovers
-	_t_dig(old_instance)
-	_i_dig(target_instance)
-	for ent in _t_nodes:
-		if !ent.Important:
+	var exporter_list = _get_all_exporters(old_instance)
+	var importer_list = _get_all_importers(target_instance)
+	
+	for exporter in exporter_list:
+		if exporter.Important == SchmoveExporter2D.ImportanceType.None:
 			continue
-		jail_node(ent, "schmove_limbo")
+		if !_t_launched.has(exporter):
+			if exporter.Important == SchmoveExporter2D.ImportanceType.Launch:
+				transition_launch(exporter)
+			if exporter.Important == SchmoveExporter2D.ImportanceType.Jail:
+				jail_node(exporter, "schmove_limbo")
+	
+	if _t_shuffle:
+		importer_list.shuffle()
+		_t_launched.shuffle()
 	
 	# Find the launchers a home
-	for launched in _t_launchers:
-		for importer in _t_importers:
-			if importer.TransitionKeyword != "" or _t_keyword != "":
-				if importer.TransitionKeyword != _t_keyword:
-					continue
-			if importer._consumed:
-				continue
+	for launched in _t_launched:
+		# ungrouped nodes are just brought over with no modification.
+		if launched.Group == 0:
+			print("inserting "+launched.get_parent().name)
+			var real = get_real_node(launched)
+			target_instance.add_child(real)
+			launched.call_deferred("_emit_transport")
+			continue
+		var found = false
+		for importer in importer_list:
 			if importer.is_compatible(launched):
+				var target_destination
+				if importer.TargetInsertionPath != null:
+					target_destination = importer.get_node(importer.TargetInsertionPath)
+				else:
+					target_destination = importer.get_parent()
 				var real = get_real_node(launched)
-				importer.get_parent().add_child(real)
+				target_destination.add_child(real)
 				real.set_deferred("global_position", importer.global_position)
 				importer._consumed = true
+				found = true
+				launched.call_deferred("_emit_transport")
 				break
+		if !found:
+			jail_node(launched, "schmove_limbo")
+			print("SCHMOVE: Failed to find a suitable home for <"+launched.Nickname+">, sent to jail.")
 	
 	get_tree().root.call_deferred("add_child", target_instance)
 	get_tree().set_deferred("current_scene", target_instance)
+	call_deferred("_emit_transition")
 	old_instance.queue_free()
 	
 	# Then use cancel to clear the statemachine.
@@ -151,37 +218,28 @@ func finish_transition():
 ## with finish_transition or cancel with cancel_transition
 func cancel_transition():
 	_t_level = ""
-	_t_nodes.clear()
-	_t_launchers.clear()
-	_t_importers.clear()
+	_t_launched.clear()
 	_t_keyword = ""
-	_using_wipe = false
-
-func unique_register(nick: String, target: Node):
-	if !_uniques.has(nick):
-		_uniques[nick] = target
-func unique_available(nick: String) -> bool:
-	if _uniques.has(nick):
-		return false
-	return true
+	_t_using_wipe = false
+	_t_shuffle = false
 
 ## Removes a node from the tree, outside of any scene, temporarily stored until
-## you want to do something with it, eg kill it, restore it, transition it,
-## or flat out just leave it in jail so it doesnt 'respawn' if it is a unique exporter.
+## you want to do something with it, eg kill it, restore it, or transition it
 func jail_node(target: Node, keyword: String = ""):
-	var real_node: Node = null
-	if target is SchmoveExporter2D:
-		_t_launchers.append(target as SchmoveExporter2D)
-		real_node = target.get_parent()
-		real_node.get_parent().remove_child(real_node)
-	else:
-		for c in target.get_children():
-			if c is SchmoveExporter2D:
-				_t_launchers.append(c)
-				real_node = c.get_parent()
-				real_node.get_parent().remove_child(real_node)
-	if real_node == null:
-		print("SCHMOVE: Failed to jail "+target.name+" as we could not find a SchmoveExporter2D in the children, and it itself is not a SchmoveExporter2D.")
+	var real_node: Node = get_real_node(target)
+	var exporter = get_exporter_node(target)
+	if !_jail.has(keyword):
+		_jail[keyword] = []
+	if real_node == null or exporter == null:
+		print("SCHMOVE: Jailed node did not have an exporter, or was not an exporter.")
 		return
-	if _jail.has(keyword):
-		_jail[keyword].append(real_node)
+	real_node.get_parent().remove_child(real_node)
+	_jail[keyword].append(exporter as SchmoveExporter2D)
+	exporter.call_deferred("_emit_jailed")
+
+## Semi expensive depending on scene search depth and how you layout your nodes
+func jail_by_name(name: String, keyword: String = ""):
+	var exporters = _get_all_exporters(get_tree().current_scene)
+	for exp in exporters:
+		if exp.Nickname == name:
+			jail_node(exp, keyword)
--- global/schmove_curtain.gd
+++ global/schmove_curtain.gd
@@ -9,24 +9,24 @@ var _scale = 0.0
 func _ready():
 	visible = false
 
-func prime_transition(material: CanvasItemMaterial, duration: float):
+func prime_transition(material: ShaderMaterial, duration: float, from: Texture2D):
 	_scale = Engine.time_scale
 	if material != null:
 		$TextureRect.material = material
 	_dur = duration
 	_timer = duration
-
-func run_transition(from: Texture2D):
 	$TextureRect.texture = from
+
+func run_transition():
 	visible = true
 	_running = true
 	
 func _process(delta):
 	if _running:
 		_timer -= delta
-		var theta = 1.0-(_timer / _dur)
-		$TextureRect.material.set_shader_parameter("progress", theta)
-		var tail_value = clamp((1.0 - (0.5/theta)) * 2.0, 0.0, 1.0)
+		var prog = 1.0-(_timer / _dur)
+		$TextureRect.material.set_shader_parameter("progress", prog)
+		#var tail_value = clamp((1.0 - (0.5/theta)) * 2.0, 0.0, 1.0)
 		
 	if _running and _timer <= 0.0:
 		visible = false
