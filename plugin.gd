401fe9426e00e5b08f9a2702dfcc44b11674a93d
JonSnowbd <vasti009@gmail.com>
2023-03-06 16:52:34 -0400

    Plugin overrides for settings, updated readme

--- plugin.gd
+++ plugin.gd
@@ -2,37 +2,33 @@
 extends EditorPlugin
 class_name SchmoveEditorPlugin
 
+
+func _schmove_initial_setting(path: String, initial_value, order: int):
+	if !ProjectSettings.has_setting(path):
+		ProjectSettings.set_setting(path, initial_value)
+	ProjectSettings.set_initial_value(path, initial_value)
+	ProjectSettings.set_order(path, order)
+
 func _schmove_layer_setting(i: int):
 	if !ProjectSettings.has_setting("layer_names/schmove/slot"+str(i)):
 		ProjectSettings.set_setting("layer_names/schmove/slot"+str(i), "Slot "+str(i))
-		ProjectSettings.set_initial_value("layer_names/schmove/slot"+str(i), "Slot "+str(i))
-		ProjectSettings.set_order("layer_names/schmove/slot"+str(i), i)
 	else:
 		var val = str(ProjectSettings.get_setting("layer_names/schmove/slot"+str(i)))
 		ProjectSettings.set_setting("layer_names/3d_navigation/layer_"+str(i), val)
+	ProjectSettings.set_initial_value("layer_names/schmove/slot"+str(i), "Slot "+str(i))
+	ProjectSettings.set_order("layer_names/schmove/slot"+str(i), i)
 
-func _schmove_layer_reprobe(i: int):
-	if !ProjectSettings.has_setting("layer_names/schmove/slot"+str(i)):
-		return
-	var cur = str(ProjectSettings.get_setting("layer_names/3d_navigation/layer_"+str(i)))
-	var sch = str(ProjectSettings.get_setting("layer_names/schmove/slot"+str(i)))
-	if cur != sch:
-		ProjectSettings.set_setting("layer_names/3d_navigation/layer_"+str(i), sch)
-
-func _schmove_on_changed_settings():
-	for i in range(32):
-		_schmove_layer_reprobe(i+1)
-
-
-var connection
 func _enter_tree():
 	# Todo: Find out how to custom name flags.
 	for i in range(32):
 		_schmove_layer_setting(i+1)
+	_schmove_initial_setting("application/schmove/search_depth", 3, 1)
+	_schmove_initial_setting("application/schmove/time_mush", 0.1, 2)
+	_schmove_initial_setting("application/schmove/time_mush_on_wipe_transition", true, 3)
 	ProjectSettings.save()
 	# ProjectSettings.connect("property_list_changed", self._schmove_on_changed_settings)
 	add_autoload_singleton("Schmove", "res://addons/schmove/global/schmove.tscn")
-	print("Thank you for using Schmove, if you're stuck on our features, hit up https://test.com")
+	print("Thank you for using Schmove, if you're stuck hit up https://github.com/JonSnowbd/Schmove")
 
 func _exit_tree():
 	remove_autoload_singleton("Schmove")
--- readme.md
+++ readme.md
@@ -14,9 +14,23 @@ that got explicitly launched (or were implicitly launched by being marked Import
 ## Features
 
 - Wipe transitions via simple shaders
-- Automatic free cross-scene entities including prevention of respawning unique nodes
-- In depth control with a Jail system
+- Automatic free cross-scene entities
+- In depth control of entities with a Jail system
 
 ## Guides
 
 Coming soon, but for now the scripts are very well documented.
+
+## Tips
+
+Turn on advanced settings in your project settings to see a schmove menu for
+both layer names, and general settings
+
+## Huh??
+
+This is exclusively a 2d aimed plugin, it will override 3d_navigation_layer settings
+in your project settings Layer Names to implement the custom named bitset flags.
+
+If things arent working, triple check that entities are near the top level,
+or configure deep_search_depth in project settings to determine how deep
+into a node nest Schmove will look for exporters
